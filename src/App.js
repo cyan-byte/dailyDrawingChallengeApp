fe8852f31729b77cf0f668d69bf5cde6e0565fd1
cyan-byte <62404444+cyan-byte@users.noreply.github.com>
2023-12-15 16:13:50 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import DrawingCanvas from './DrawingCanvas';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Daily Drawing Challenge App</h1>
+      </header>
+      <DrawingCanvas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DrawingCanvas.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./DrawingCanvas.css";
+
+const DrawingCanvas = () => {
+  return (
+    <div>
+      <h2>Drawing Canvas</h2>
+      <div className="drawing-canvas">{/* Add your drawing canvas here */}</div>
+    </div>
+  );
+};
+
+export default DrawingCanvas;
